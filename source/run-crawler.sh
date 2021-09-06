50766ef7d93286b76152c4b4eae50690fcd61666
aster-max <icatmuhammad2@gmail.com>
2021-09-06 23:39:18 +0700

    refactoring files

--- source/run-crawler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#----------
-GREEN="\e[32m"
-YELLOW="\e[33m"
-ENDCOLOR="\e[0m"
-#----------
-## get status code of each link    : curl -q --head https://hotpotcookie.github.io/index.html | grep "HTTP" | cut -d ' ' -f 2
-## get potential directory to file : curl https://hotpotcookie.github.io | grep "href=" | cut -d '"' -f 2 | sort | uniq
