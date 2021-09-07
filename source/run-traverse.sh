3f8b063baa229697120a475acb65a501376d5917
aster-max <icatmuhammad2@gmail.com>
2021-09-08 00:37:58 +0700

    need to use user-agent

--- source/run-traverse.sh
+++ source/run-traverse.sh
@@ -6,11 +6,13 @@ ENDCOLOR="\e[0m"
 #----------
 scrap="log/scrap.log"
 traverse="log/traverse.log"
+main_tmp="log/main.log.tmp"
 main="log/main.log"
 #----------
 url="$1"
 cat /dev/null > "$scrap"
 cat /dev/null > "$traverse"
+cat /dev/null > "$main_tmp"
 #----------
 php source/run-crawler.php "$url" | sort | uniq > "$scrap"
 while IFS= read -r each_dir; do
@@ -18,6 +20,6 @@ while IFS= read -r each_dir; do
 	## sort uniq ke main.log
 	echo "crawling: $each_dir"
 	php source/run-crawler.php "$each_dir" | sort | uniq > "$traverse" & wait
-	cat "$traverse" >> "$main.tmp" & wait
-	cat "$main.tmp" | sort | uniq > "$main" & wait
+	cat "$traverse" >> "$main_tmp" & wait
+	cat "$main_tmp" | sort | uniq > "$main" & wait
 done < "$scrap"
