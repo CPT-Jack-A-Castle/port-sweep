c7218022469855d316bf6419904c040e9da57b20
aster-max <icatmuhammad2@gmail.com>
2021-09-19 14:42:31 +0700

    main script on probing ports. about to tested on Kali, not working in WSL

---
+++ source/run-scanner.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#----------
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[36m"
+ENDCOLOR="\e[0m"
+#----------
+t="$1"
+p="$2"
+o="$3"
+#----------
+trapexit() {
+	duration=$SECONDS		
+	printf "\r\n${RED}[sweep]${ENDCOLOR}: ---"
+	printf "\n${RED}[sweep]${ENDCOLOR}: Port-Sweep cancelled for $(($duration / 60)) minutes $((duration % 60)) seconds"	
+	echo ""
+	exit 0
+}
+trap trapexit SIGINT
+#----------
+check_curl=$(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed")
+check_nmap=$(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed")
+check_ncat=$(dpkg-query -W -f='${Status}' ncat 2>/dev/null | grep -c "ok installed")
+if [[ "$check_nmap" -eq 0 || "$check_ncat" -eq 0 || "$check_curl" -eq 0 ]]; then
+	echo -e "${YELLOW}[sweep]${ENDCOLOR}: You have unmet dependencies";
+	echo -e "${YELLOW}[sweep]${ENDCOLOR}: Use flag -i for auto installation";
+	exit 0
+else
+	#----------
+	datetime=$(date +"%d/%m/%y %T")
+	SECONDS=0
+	echo -e "${BLUE}[sweep]${ENDCOLOR}: Starting Port-Sweep 2.12 at $datetime ..."
+	#----------
+	check_host=$(curl -I "$t" 2>&1 | grep "Failed")
+	if [[ "$check_host" ]]; then
+		echo -e "${RED}[sweep]${ENDCOLOR}: Host seems is inactive. Aborting"	
+		exit 0
+	fi
+	#----------
+	IFS='-' read -r -a port <<< "$p"
+	if [[ ! "${port[1]}" ]]; then
+		port[1]="${port[0]}"
+	else
+		if [[ "${port[0]}" -gt "${port[1]}" ]]; then
+			port_temp="${port[0]}"
+			port[0]="${port[1]}"
+			port[1]="$port_temp"
+		fi
+	fi
+	nc -zvv "$t" "${port[0]}" 2>&1 | grep "succeeded" & wait
+	duration=$SECONDS
+	range=$((${port[1]} - ${port[0]}))
+	estimate=$(($duration * $range))
+	echo -e "${BLUE}[sweep]${ENDCOLOR}: Probing estimation finished in $(($estimate / 60)) minutes $((estimate % 60)) seconds ..."
+	echo -e "${BLUE}[sweep]${ENDCOLOR}: ---"
+	SECONDS=0
+	#----------
+	for probe in $(seq "${port[0]}" "${port[1]}"); do
+		result=$(nc -zvv "$t" "$probe" 2>&1 | grep "succeeded" & wait)
+		if [[ "$result" ]]; then
+			echo -e "${GREEN}[sweep]${ENDCOLOR}: $result"
+			nmap -sS -sV "$t" -p "$probe" -T5
+			if [[ "$probe" -eq "${port[1]}" ]]; then
+				duration=$SECONDS					
+				sleep 2s & wait
+				echo -e "${BLUE}[sweep]${ENDCOLOR}: Port-Sweep done for $(($duration / 60)) minutes $((duration % 60)) seconds"
+			fi
+			continue
+		fi
+		echo -e "${YELLOW}[sweep]${ENDCOLOR}: Probing port at $probe ! ...";
+		if [[ "$probe" -eq "${port[1]}" ]]; then
+			duration=$SECONDS				
+			sleep 2s & wait
+			printf "\033[A"
+			echo -e "\n${BLUE}[sweep]${ENDCOLOR}: ---"		
+			echo -e "${BLUE}[sweep]${ENDCOLOR}: Port-Sweep done for $(($duration / 60)) minutes $((duration % 60)) seconds"
+			echo ""
+		fi
+		printf "\033[A"
+	done
+fi
