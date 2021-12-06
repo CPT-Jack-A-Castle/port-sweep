ff13ec63261c2d5b2f7376e2336e6bb32c028e04
aster-max <icatmuhammad2@gmail.com>
2021-12-06 23:00:53 +0700

    moved into a comment section alongside with scanner

--- ok.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-test=0
-a="1-20,80,443,3306"
-IFS=',' read -r -a segment <<< "$a"
-for range in "${segment[@]}"; do
-	test=$((++test))
-	echo "$range"
-done
-echo "${segment[${#segment[@]}-1]}"
