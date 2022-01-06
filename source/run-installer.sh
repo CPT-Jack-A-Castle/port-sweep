41545e4c8ca451ec23491b318b5872673990d286
aster-max <icatmuhammad2@gmail.com>
2022-01-06 10:07:29 +0700

    migrating to win again

--- source/run-installer.sh
+++ source/run-installer.sh
@@ -8,6 +8,7 @@ ENDCOLOR="\e[0m"
 #----------
 check_curl=$(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed")
 check_nmap=$(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed")
+check_ping=$(dpkg-query -W -f='${Status}' iputils-ping 2>/dev/null | grep -c "ok installed")
 check_ncat=$(dpkg-query -W -f='${Status}' netcat-openbsd 2>/dev/null | grep -c "ok installed")
 check_tor=$(dpkg-query -W -f='${Status}' tor 2>/dev/null | grep -c "ok installed")
 check_proxychains=$(dpkg-query -W -f='${Status}' proxychains 2>/dev/null | grep -c "ok installed")
@@ -16,12 +17,12 @@ echo -e "${YELLOW}[sweep]${ENDCOLOR}: Updating Packages ...\n---"
 apt-get update -y & wait
 echo "---"			
 #----------	
-if [[ "$check_nmap" -eq 0 ]]; then
-	echo -e "${YELLOW}[sweep]${ENDCOLOR}: Installing nmap ...\n---"		
-	apt-get install nmap -y & wait
+if [[ "$check_ping" -eq 0 ]]; then
+	echo -e "${YELLOW}[sweep]${ENDCOLOR}: Installing iputils-ping ...\n---"		
+	apt-get install iputils-ping -y & wait
 	echo "---"		
 else
-	echo -e "${YELLOW}[sweep]${ENDCOLOR}: nmap have been installed ..."				
+	echo -e "${YELLOW}[sweep]${ENDCOLOR}: iputils-ping have been installed ..."				
 fi
 if [[ "$check_curl" -eq 0 ]]; then
 	echo -e "${YELLOW}[sweep]${ENDCOLOR}: Installing curl ...\n---"		
